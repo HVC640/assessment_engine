1753671666e8d76653e0fd6ecd1eb82a53c23463
Harsh Chaudhari <harshc640@gmail.com>
2025-08-31 12:31:12 +0530

    Add logging configuration and setup for improved logging management

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import uvicorn
+import logging
+import logging.config
+import os
+
+if os.path.exists("core/logging_config.ini"):
+    logging.config.fileConfig("core/logging_config.ini", disable_existing_loggers=False)
+else:
+    logging.basicConfig(level=logging.INFO)
+    logging.warning("logging_config.ini not found. Using basicConfig.")
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
